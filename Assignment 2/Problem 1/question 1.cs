413239615c895b77fc46b8d734fc2c23c43c2f00
MominKhan9 <mominnadeem66@gmail.com>
2022-12-25 19:52:34 +0500

    update

---
+++ Assignment 2/Problem 1/question 1.cs
@@ -0,0 +1,33 @@
+sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace sir_farooq_asigment
+{
+ internal class Program
+ {
+ static void Main(string[] args) 
+ {
+
+ Console.WriteLine(CalculatePersistence());
+ int CalculatePersistence()
+ {
+ Console.WriteLine("Enter a number: ");
+ int number = int.Parse(Console.ReadLine());
+ int persistence = 0;
+ while (number > 9)
+ {
+ int result = 1;
+foreach (char c in number.ToString())
+ {
+ result *= int.Parse(c.ToString());
+ }
+number = result;
+persistence++;
+ }
+ return persistence;
+ }
+ }
+ }
+}
