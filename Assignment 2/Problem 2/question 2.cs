cad0704d82dc65c3cc2000211bcf8ae836ee1057
Momin Khan <117566652+MominKhan9@users.noreply.github.com>
2023-01-02 19:59:37 +0500

    update

--- Assignment 2/Problem 2/question 2.cs
+++ Assignment 2/Problem 2/question 2.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace sir_farooq_asigment
+namespace assignment_2
 {
  internal class Program
  {
