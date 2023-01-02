cf590f30fce8498d8361a03541443581e699f963
Momin Khan <117566652+MominKhan9@users.noreply.github.com>
2023-01-02 19:11:46 +0500

    update

--- Assignment 2/Problem 5/question 5.cs
+++ Assignment 2/Problem 5/question 5.cs
@@ -12,10 +12,10 @@ namespace sir_farooq_asigment
  static void Main(string[] args)
  {
  int n1=0,n2=1,n3,i,number;    
-         Console.Write("Enter the number of elements: ");    
+         Console.Write("Enter nth term you want: ");    
          number = int.Parse(Console.ReadLine());  
-         Console.Write(n1+" "+n2+" "); //printing 0 and 1    
-         for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+         Console.Write(n1+" "+n2+" ");     
+         for(i=2;i<number;++i)   
          {    
           n3=n1+n2;    
           Console.Write(n3+" ");    
