5b9ca062f3888d1c76af4aaf40c316f3ba3d684b
Momin Khan <117566652+MominKhan9@users.noreply.github.com>
2023-01-03 16:21:41 +0500

    update

--- Assignment 2/Problem 3/question 3.cs
+++ Assignment 2/Problem 3/question 3.cs
@@ -5,22 +5,30 @@ using System.Linq;
 using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace sir_farooq_asigment
 {
- internal class Program
- {
- static void Main(string[] args)
- {
+    internal class Program
+    {
 
- 
- Console.WriteLine("Enter a number to calculate its factorial:");
- int num = int.Parse(Console.ReadLine());
- int factorial = 1;
- for (int i = 1; i <= num; i++)
- {
- factorial = factorial + (factorial * (i - 1));
- }
- Console.WriteLine("The factorial of " + num + " is " + factorial);
- }
- }
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Enter a number to calculate its factorial:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fac = num;
+
+            for (int i = num -1; i >0; i--)
+            {
+               int sum =0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum=sum+fac;
+                }
+                fac = sum;
+            }
+
+            Console.WriteLine("The factorial of " + num + " is " + fac);
+            Console.ReadKey();
+        }
+    }
 }
